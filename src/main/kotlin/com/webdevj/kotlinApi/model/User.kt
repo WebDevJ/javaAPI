e365140336cf140f06585298b872edf7fa1bd322
WebDevJ <WebDevJ@users.noreply.github.com>
2021-05-31 23:35:47 -0400

    data layer with mock data source

--- src/main/kotlin/com/webdevj/kotlinApi/model/User.kt
+++ src/main/kotlin/com/webdevj/kotlinApi/model/User.kt
@@ -2,6 +2,6 @@ package com.webdevj.kotlinApi.model
 
 // data transfer object
 data class User (
-    val studentid: String,
-    val studentName: Int
+    val studentid: Int,
+    val studentName: String
 )
